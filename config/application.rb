d01a82969a2b7667be64a9271acd8a57b865cf7b
Kevin <litchfield.kevin+gs@gmail.com>
2015-02-06 03:43:01 -0800

    Theoretically config.assets.initialize_on_precompile not available in rails 4

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module LearnAsciidoc
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.assets.initalialize_on_precompile = false
+    # config.assets.initalialize_on_precompile = false
   end
 end
